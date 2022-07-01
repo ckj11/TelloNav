51aa49956a71c4373b067cbcf86c5234a071e537
Coral_16_User <coral@coral-16.mshome.net>
2022-07-01 15:41:57 -0500

    Added Testing simple velocity moving and ROS advertising

--- TelloNav/droneNav/src/control_plugin.cpp
+++ TelloNav/droneNav/src/control_plugin.cpp
@@ -5,6 +5,9 @@
 #include <ignition/math/Pose3.hh>
 
 #include <ros/ros.h>
+#include "std_msgs/String.h"
+#include <sstream>
+
 
 namespace gazebo {
 
@@ -16,15 +19,44 @@ namespace gazebo {
                 this->update_connection = event::Events::ConnectWorldUpdateBegin(
                     std::bind(&TelloControl::update, this));
 
-                this->currentPosition = this->model->WorldPose();
 
                 this->currentSpeed = ignition::math::Vector3d(0, 0, 0);
 
-                this->targetPosition = ignition::math::Pose3d(0, 0, 0, 0, 0, 0);
+                this->targetPosition = ignition::math::Pose3d(5, 5, 5, 0, 0, 0);
+
+                this->pub_pos = n.advertise<std_msgs::String>("location", 10);
             }
 
             void update() {
-                
+                this->currentPosition = this->model->WorldPose();
+                ss << currentPosition.Pos().X() << " " << currentPosition.Pos().Y() << " " << currentPosition.Pos().Z();
+                msg.data = ss.str();
+                pub_pos.publish(msg);
+                ss.str("");
+                ss.clear();
+
+            if(this->targetPosition.Pos().X() > this->model->WorldPose().Pos().X()) {
+                this->currentSpeed.X(0.3);
+            }
+            else {
+                this->currentSpeed.X(0);
+            }
+
+            if(this->targetPosition.Pos().Y() > this->model->WorldPose().Pos().Y()) {
+                this->currentSpeed.Y(0.3);
+            }
+            else {
+                this->currentSpeed.Y(0);
+            }
+            
+            if(this->targetPosition.Pos().Z() > this->model->WorldPose().Pos().Z()) {
+                this->currentSpeed.Z(0.3);
+            }
+            else {
+                this->currentSpeed.Z(0);
+            }
+            
+            this->model->SetLinearVel(this->currentSpeed);
             }
 
         private:
@@ -35,7 +67,12 @@ namespace gazebo {
             ignition::math::Pose3d currentPosition;
             ignition::math::Vector3d currentSpeed;
 
-            
+            ros::Publisher pub_pos;
+            ros::NodeHandle n;
+            std_msgs::String msg;
+
+            std::stringstream ss;
+
 
 
 
