088578580f2f0f28f2857a979134d7779c9f29b6
Coral_16_User <coral@coral-16.mshome.net>
2022-07-12 10:43:42 -0500

    Working position control with rotation as well

--- TelloNav/droneNav/src/control_plugin.cpp
+++ TelloNav/droneNav/src/control_plugin.cpp
@@ -1,6 +1,7 @@
 #include <thread>
 #include <gazebo/gazebo.hh>
 #include <gazebo/physics/physics.hh>
+#include <gazebo/common/common.hh>
 
 #include <ignition/math/Vector3.hh>
 #include <ignition/math/Pose3.hh>
@@ -13,10 +14,13 @@
 #include "std_msgs/Float32.h"
 #include "geometry_msgs/Pose.h"
 #include "geometry_msgs/Twist.h"
+#include "rosgraph_msgs/Clock.h"
 
 #include <sstream>
 #include <string>
 #include <iostream>
+#include <vector>
+#include "../include/droneNav/pid.hpp"
 
 
 namespace gazebo {
@@ -30,10 +34,25 @@ namespace gazebo {
                     ros::init(argc, argv, "Tello",
                     ros::init_options::NoSigintHandler);
                 }
-                
-                this->model = _parent;
+                if (_sdf->HasElement("center_of_mass")) {
+                    centerOfMass = _sdf->GetElement("center_of_mass")->Get<ignition::math::Vector3d>();
+                }
 
-                this->body = this->model->GetLink((std::string) "Tello::link_0_clone");
+                this->model = _parent;
+                this->gravity = this->model->GetWorld()->Gravity();
+
+                //Simulation tuning for gravity cancelation
+                //This happens because this update functions runs faster than the simulation so the gravity vector is applied twice
+                double factor = 0.5004;
+                gravity *= factor;
+                
+                std::cout << "gravity vector: " << gravity.X() << " " << gravity.Y() << " " << gravity.Z() << std::endl;
+                //Fetch prop links
+                this->body = this->model->GetLink((std::string)"Tello::body");
+                this->prop1 = this->model->GetLink((std::string)"Tello::prop_1");
+                this->prop2 = this->model->GetLink((std::string)"Tello::prop_2");
+                this->prop3 = this->model->GetLink((std::string)"Tello::prop_3");
+                this->prop4 = this->model->GetLink((std::string)"Tello::prop_4");
 
                 this->rosNode.reset(new ros::NodeHandle("/Tello"));
 
@@ -48,71 +67,88 @@ namespace gazebo {
                 //Subscribe to topic using above options
                 this->sub_target = this->rosNode->subscribe(so);
 
-                //Set subscriber options for target topic
-                ros::SubscribeOptions so2 = 
-                ros::SubscribeOptions::create<std_msgs::Float32>(
-                "/" + this->model->GetName() + "/speed",
-                1,
-                boost::bind(&TelloControl::OnRosSpeedMsg, this, _1),
-                ros::VoidPtr(), &this->rosSpeedQueue);
-
-                this->sub_speed = this->rosNode->subscribe(so2);
-                
                 //Set up target queue helper thread
                 this->rosTargetQueueThread = std::thread(std::bind(&TelloControl::QueueTargetThread, this));
 
                 //Set up speed queue helper thread
-                this->rosSpeedQueueThread = std::thread(std::bind(&TelloControl::QueueSpeedThread, this));
 
                 this->update_connection = event::Events::ConnectWorldUpdateBegin(
                     std::bind(&TelloControl::update, this));
 
-                this->targetPosition = ignition::math::Pose3d(1, 1, 1, 0, 0, 0);
+                this->targetPosition = ignition::math::Pose3d(0, 0, 1, 0, 0, 0);
 
-                this->currentSpeed = 10;
+                set_target_velocities(0, 0, 1, 0);
+
+                this->rotationSpeed = 1;
 
                 this->pub_pos = rosNode->advertise<std_msgs::String>("location", 10);
-
-                
             }
 
             void update() {
-                this->currentPosition = this->model->WorldPose();
+                this->currentPosition = this->body->WorldPose();
                 ss << currentPosition.Pos().X() << " " << currentPosition.Pos().Y() << " " << currentPosition.Pos().Z();
                 msg.data = ss.str();
                 pub_pos.publish(msg);
                 ss.str("");
                 ss.clear();
 
-                //Calculate heading to next point
-                ignition::math::Vector3d heading = (targetPosition - currentPosition).Pos();
+                //Calculate the dt
+                double dt = (ros::Time::now() - prevTime).toSec();
+                if(dt == 0) {
+                    dt = 0.001;
+                }
+                //double dt = 0.001;
+                //Get velocities
+                ignition::math::Vector3d linear_velocity = body->RelativeLinearVel();
+                ignition::math::Vector3d angular_velocity = body->RelativeAngularVel();
+                
+                //Calc desired accelerations(ubar)
+                ignition::math::Vector3d lin_ubar, ang_ubar;
+                lin_ubar.X(xController.calc(this->currentPosition.Pos().X(), dt, 0));
+                lin_ubar.Y(yController.calc(this->currentPosition.Pos().Y(), dt, 0));
+                lin_ubar.Z(zController.calc(this->currentPosition.Pos().Z(), dt, 0));
+
+                // Compensate for gravity
+                lin_ubar -= gravity;
+                
+                //Calc force and torque
+                ignition::math::Vector3d force = lin_ubar * body->GetInertial()->Mass();
+                ignition::math::Vector3d torque = ang_ubar * body->GetInertial()->MOI();
+
+                //Set roll and pitch to zero
+                ignition::math::Pose3d pose = body->WorldPose();
+                pose.Rot().X(0);
+                pose.Rot().Y(0);
+                body->SetWorldPose(pose);
+
+                body->AddForce(force);
+                body->AddRelativeTorque(torque);
+
+                //Calculate vector for rotation
                 ignition::math::Vector3d rotation = (targetPosition - currentPosition).Rot().Euler();
 
-                //Get unit vector
-                heading.Normalize();
                 rotation.Normalize();
 
                 //Multiply speed to get velocity vector to the target
-                heading *= currentSpeed;
-                rotation *= currentSpeed;
+                rotation *= rotationSpeed;
                 
                 //give model that set speed
-                this->model->SetLinearVel(heading);
                 this->model->SetAngularVel(rotation);
 
+                //give props angular velocity to spin
+                SpinUpProps();
+
+                //Update time for dt
+                prevTime = ros::Time::now();
             }
 
         void OnRosTargetMsg(const geometry_msgs::TwistConstPtr &_msg) {
-            targetPosition.Pos().X(_msg->linear.x);
-            targetPosition.Pos().Y(_msg->linear.y);
-            targetPosition.Pos().Z(_msg->linear.z);
-
+            ignition::math::Vector3d pos(_msg->linear.x, _msg->linear.y, _msg->linear.z);
             ignition::math::Vector3d rot(_msg->angular.x, _msg->angular.y, _msg->angular.z);
-            targetPosition.Rot().Euler(rot);
-        }
+            targetPosition.Set(pos,rot);
 
-        public: void OnRosSpeedMsg(const std_msgs::Float32ConstPtr &_msg) {
-            currentSpeed = _msg->data;
+            set_target_velocities(_msg->linear.x, _msg->linear.y, _msg->linear.z, _msg->angular.z);
+            
         }
 
         void QueueTargetThread() {
@@ -122,11 +158,32 @@ namespace gazebo {
             }
         }
 
-        private: void QueueSpeedThread() {
-            static const double timeout = 0.01;
-            while(this->rosNode->ok()) {
-                this->rosSpeedQueue.callAvailable(ros::WallDuration(timeout));
-            }
+        void SpinUpProps() {
+            ignition::math::Vector3d tor(0, 0, 8);
+            this->prop1->SetAngularVel(tor);
+            this->prop2->SetAngularVel(tor);
+            this->prop3->SetAngularVel(tor);
+            this->prop4->SetAngularVel(tor);
+        }
+
+        void StopProps() {
+            ignition::math::Vector3d tor(0, 0, 0);
+            this->prop1->SetTorque(tor);
+            this->prop2->SetTorque(tor);
+            this->prop3->SetTorque(tor);
+            this->prop4->SetTorque(tor);
+        }
+
+         void set_target_velocities(double x, double y, double z, double yaw)
+        {
+            xController.set_target(x);
+            yController.set_target(y);
+            zController.set_target(z);
+        }
+
+        inline double clamp(const double v, const double max)
+        {
+            return v > max ? max : (v < -max ? -max : v);
         }
 
         private:
@@ -135,12 +192,19 @@ namespace gazebo {
 
             //Link ptr for main body
             physics::LinkPtr body;
+            physics::LinkPtr prop1;
+            physics::LinkPtr prop2;
+            physics::LinkPtr prop3;
+            physics::LinkPtr prop4;
             
             event::ConnectionPtr update_connection;
 
             ignition::math::Pose3d targetPosition;
             ignition::math::Pose3d currentPosition;
-            float currentSpeed;
+            ignition::math::Vector3d gravity;
+            ignition::math::Vector3d centerOfMass{0, 0, 0};
+            float rotationSpeed;
+            float currentClock;
 
             //Publisher object
             ros::Publisher pub_pos;
@@ -152,17 +216,32 @@ namespace gazebo {
             //Node handle for publishing and subscribing
             std::unique_ptr<ros::NodeHandle> rosNode;
 
-            //Callback queue for messages
+            //Callback queues for messages
             ros::CallbackQueue rosTargetQueue;
             ros::CallbackQueue rosSpeedQueue;
 
-            //Thread for callback queue
+            //Threads for callback queue
             std::thread rosTargetQueueThread;
             std::thread rosSpeedQueueThread;
 
             std_msgs::String msg;
 
             std::stringstream ss;
+            
+            //PID controllers for axis for Tello
+            pid::Controller xController{false, 0.1, 0, 0.5};
+            pid::Controller yController{false, 0.1, 0, 0.5};
+            pid::Controller zController{false, 0.1, 0.02, 1};
+
+            ros::Time prevTime;
+
+            const double MAX_XY_V = 8.0;
+            const double MAX_Z_V = 4.0;
+            const double MAX_ANG_V = M_PI;
+
+            const double MAX_XY_A = 8.0;
+            const double MAX_Z_A = 4.0;
+            const double MAX_ANG_A = M_PI;
 
 
 
